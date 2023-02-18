d9526421bba8316c57775de0318f337fb131d915
saanvi-gupta26 <91875393+saanvi-gupta26@users.noreply.github.com>
2023-02-18 22:45:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+nosex=0;
+nosey=0;
+
+function preload(){
+    image=loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+    
+    function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modeloader());
+     poseNet.on('pose',gotposes());
+    }
+    function draw(){
+        image(video,0,0,300,300);
+        
+    }
+    function button(){
+    save('photo.png');
+    }
+    function modeloader(){
+        console.log("posenet is working");
+    }
+    function gotposes(results){
+        if(results.length>0)
+    console.log(results);
+    nosex=results[0].pose.nose.x;
+ nosey=results[0].pose.nose.y;
+    }
